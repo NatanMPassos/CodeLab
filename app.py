3143947ea4526a1d4667fea319574ad1c87ede37
natanmrrpassos <natanmrrpassos@gmail.com>
2026-06-26 02:56:20 -0300

    app.py and executor.py integration done; show results still pending; choose json still pending

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask, request
+from executor import run_tests
 import json
 
-# Flask(): Inicializa a aplicação Flask
+#initialize flask
 app = Flask(__name__)
 
-# @app.route(): Define a rota/caminho que o usuário vai acessar.
-# Nesse caso, a rota "/" representa a página inicial.
 @app.route("/")
 def home():
-    # return: Retorna a resposta que será exibida no navegador do usuário.
     return "<h1>hello world</h1>"
 
 @app.route("/exercise/<int:id>", methods=['GET', 'POST'])
@@ -21,18 +19,19 @@ def exercise(id):
 
     if request.method == 'POST':
         code = request.form['code']
-        print(code)
-        return "Code received"
+        test_cases = exercise_dictionary['test_cases']
+        results = run_tests(code, test_cases)
+        return str(results)
+
     else:
         return f"""
         <h2>{exercise_dictionary['exercise']}</h2>
         <form method="POST">
             <textarea name="code"></textarea>
-            <button type="submit">Enviar</button>
+            <button type="submit">Submit</button>
         </form>
         """
 
-# app.run(): Executa o servidor de desenvolvimento local
 if __name__ == "__main__":
     app.run(debug=True)
 
--- executor.py
+++ executor.py
@@ -5,54 +5,56 @@ import subprocess
 #find python path
 python_path = sys.executable
 
-#open json
-with open('exercises.json', 'r', encoding='utf-8') as file:
-    exercise_list = json.load(file)
+def run_tests(code, test_cases):
+    right_answers = 0
+    results_dictionary = []
 
-exercise_id = int(input("Which exercise do you want to answer? Type a number: "))
-exercise_id += 1
+    #run each test case
+    for case in test_cases:
+        try:
+            results = subprocess.run(
+                [sys.executable, '-c', code],
+                input= case['test_input'],
+                capture_output=True, 
+                text=True,
+                encoding='cp1252',
+                timeout=5
+            )
 
-#separate json 
-exercise = exercise_list[exercise_id]['exercise']
-case_list = exercise_list[exercise_id]['test_cases']
+        #timeout treatment
+        except subprocess.TimeoutExpired:
+            print("Input:",case['test_input'])
+            print("Timeout error: answer code took too long to run.\n")
+            continue
 
-right_answers = 0
-total_cases = len(case_list)
+        #show answer code errors (if any)
+        if results.returncode != 0:
+            print("Input:",case['test_input'])
+            print("Answer code error:")
+            print(results.stderr)
+            continue
 
-#run each test case
-for case in case_list:
-    try:
-        results = subprocess.run(
-            [sys.executable, 'other_code.py'],
-            input= case['test_input'],
-            capture_output=True, 
-            text=True,
-            encoding='cp1252',
-            timeout=5
-        )
-    #timeout treatment
-    except subprocess.TimeoutExpired:
+        #show input and output 
         print("Input:",case['test_input'])
-        print("Timeout error: answer code took too long to run.\n")
-        continue
+        print("Expected output:",case['expected_output'].strip())
+        print("Given output:", results.stdout.strip())
         
-    #show answer code errors (if any)
-    if results.returncode != 0:
-        print("Input:",case['test_input'])
-        print("Answer code error:")
-        print(results.stderr)
-        continue
+        #compare given answer with expected result
+        if (results.stdout.strip() == case['expected_output'].strip()):
+            print ("Correct!\n")
+            right_answers += 1
+            answer_check = True
+        else:
+            print ("Incorrect!\n")
+            answer_check = False
 
-    #show input and output 
-    print("Input:",case['test_input'])
-    print("Expected output:",case['expected_output'].strip())
-    print("Given output:", results.stdout.strip())
-    
-    #compare given answer with expected result
-    if (results.stdout.strip() == case['expected_output'].strip()):
-        print ("Correct!\n")
-        right_answers += 1
-    else:
-        print ("Incorrect!\n")
+        #return all test cases results
+        results_dictionary.append({
+            "input": case['test_input'],
+            "expected": case['expected_output'].strip(),
+            "got": results.stdout.strip(),
+            "passed": answer_check
+        })
 
-print(f"You got {right_answers} out of {total_cases} cases right!")
+    return results_dictionary
+
